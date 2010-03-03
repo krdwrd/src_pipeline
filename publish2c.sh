fff9e1ba03194c9b96b60adf8d3dc4bf3a9fab67
Egon W. Stemle <egon@krdwrd.org>
2010-03-03 18:39:19 +0000

    make sure to even sort large numnbers correctly; minor logging changes

--- publish2c.sh
+++ publish2c.sh
@@ -23,19 +23,21 @@ gt=0
 test -f $FILELIST && rm $FILELIST
 test -f $FILELIST.no && rm $FILELIST.no
 
-for i in $(find $(dirname ${SRC})/$(basename ${SRC})/ -name '*.log' | sort -n)
+for i in $(find $(dirname ${SRC})/$(basename ${SRC})/ -name '*.log' -printf '%f\n' | sort -n)
 do
     let log++
 
     # be verbose about progress every now and then
     if ! (( $log % 5000 )); then echo -n "${log} "; fi
 
-    B=${i%.log}
+    fid=${i%.log}
+    B=${SRC}/${fid}
+
     # check for the output files of an app run - without these we need not proceed
     [ -e ${B}.gold ] && [ -e ${B}.cl ] && [ -e ${B}.txt ] && [ -e ${B}.dom ] && [ -e ${B}.bte ] && [ -e ${B}.xy2 ] || \
     { 
         let nlog++
-        echo "${B##*/} $URL ${W[0]} ${W[1]} not:A" >> $FILELIST.no
+        echo "${fid} not:A" >> $FILELIST.no
         continue
     }
    
@@ -51,28 +53,28 @@ do
             [ -z $(grep -l -E "(WWWOFFLE)|(Not Found.*404)|(404.*Not Found)|(Error.*404)|(404.*Error)" ${B}.cl ) ] || \
             {
                 let lt++
-                echo "${B##*/} $URL ${W[0]} ${W[1]} err:404" >> $FILELIST.no
+                echo "${fid} err:404 ${W[0]} ${W[1]}" >> $FILELIST.no
                 continue
             }
         
                 # file has > 200.000 chars? (~200kB) then skip it
         elif [[ "${W[1]}" -gt 10000 ]]
         then
-            echo "${B##*/} $URL ${W[0]} ${W[1]} gt:$W" >> $FILELIST.no
+            echo "${fid} gt: ${W[0]} ${W[1]}" >> $FILELIST.no
             let gt++
             continue
         fi
 
-        URL=$(awk '/^URL:/ { print $2; }' < $i | tail -n 1)
+        URL=$(awk '/^URL:/ { print $2; }' < ${B}.log | tail -n 1)
         if [ "${URL:0:4}" = "http" ]
         then
             let cnt++
-            echo "${B##*/} $URL" >> $FILELIST
+            echo "${fid} $URL" >> $FILELIST
         else
-            echo "${B##*/} $URL ${W[0]} ${W[1]} err:url" >> $FILELIST.no
+            echo "${fid} err:url ${W[0]} ${W[1]} $URL" >> $FILELIST.no
         fi
     else
-        echo "${B##*/} $URL ${W[0]} ${W[1]} lt:${W[1]}" >> $FILELIST.no
+        echo "${fid} lt: ${W[0]} ${W[1]}" >> $FILELIST.no
         let lt++
     fi
 done
