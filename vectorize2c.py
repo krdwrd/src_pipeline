9920a58df1c77dc4037e3128afbbd3d9e59c048a
Egon W. Stemle <egon@krdwrd.org>
2010-03-09 16:04:03 +0000

    fixed syntax error

--- vectorize2c.py
+++ vectorize2c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 from sys import stdin
 for l in stdin.xreadlines():
-    print = "0 " + " ".join(["%d:%f" % (i + 1, float(f)) for i, f in enumerate(l.split())])
+    print "0 " + " ".join(["%d:%f" % (i + 1, float(f)) for i, f in enumerate(l.split())])
