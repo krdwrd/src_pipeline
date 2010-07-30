0f976e436bdccaf5d73b92254b0e3c30a806f9d9
Egon W. Stemle <egon@krdwrd.org>
2010-07-30 20:06:44 +0000

    make error logic work

--- bte.sh
+++ bte.sh
@@ -2,6 +2,6 @@
 #
 # use the krdwrd's BTE pipe output and filter the TXT pipe output accrodingly
 
-[ -n $1 ] && { echo "Usage: $(basename $0) pipeout.bte (with pipeout.txt in the same directory.)" ; exit 1;}
+[ -z $1 ] && { echo "Usage: $(basename $0) pipeout.bte (with pipeout.txt in the same directory.)" ; exit 1;}
 
 paste -d ' ' ${1} $(dirname ${1})/$(basename ${1} .bte).txt | grep -E "^[0-9]+ [0-9]+ 1 " | cut -d ' ' -f 4- | uniq
