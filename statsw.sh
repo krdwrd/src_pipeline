68c1999a98788952427b43f8c2be1caf3f015109
Egon W. Stemle <egon@krdwrd.org>
2010-08-03 16:34:48 +0000

    different format for stats - put numbers first for quick comparision

--- statsw.sh
+++ statsw.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
 
 for i in $1/*.allvecs.scaled.1.test.*.resw; do
-    echo -n "$i "; for a in `echo $i | sed s/1/\*/`; do tail -n 3 $a | tr '\n' '\t';echo; done | awk '{a+=$1; b+=$2; c+=$3;}END{print a/10, b/10, c/10}'
+    for a in $(echo $i | sed s/1/\*/)
+    do 
+        tail -n 3 $a | tr '\n' '\t'    
+        echo
+    done | awk '{a+=$1; b+=$2; c+=$3;}END{printf "%0.5f %.5f %0.5f", a/10, b/10, c/10}'
+    echo " ${i##*/}"
 done
