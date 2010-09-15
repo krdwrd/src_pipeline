cb1ca7490f96eb72cc021fa6422b14ce1a2ac805
Egon W. Stemle <egon@krdwrd.org>
2010-09-15 14:27:00 +0000

    cleaning - minor

--- bte.sh
+++ bte.sh
@@ -4,4 +4,4 @@
 
 [ -z $1 ] && { echo "Usage: $(basename $0) pipeout.bte (with pipeout.txt in the same directory.)" ; exit 1;}
 
-paste -d ' ' ${1} $(dirname ${1})/$(basename ${1} .bte).txt | grep -E "^[0-9]+ [0-9]+ 1 " | cut -d ' ' -f 4- | uniq
+paste -d ' ' <(cut -d ' ' -f 3 ${1}) $(dirname ${1})/$(basename ${1} .bte).txt | grep -E "^1 " | cut -d ' ' -f 2- | uniq
