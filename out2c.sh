2454539d1762b6f40c3ab90db88e7f48c31adbda
Egon W. Stemle <egon@krdwrd.org>
2010-09-15 09:10:24 +0000

    late: cleaning - adapt 'vectorization' and scaling to work-flow with a publish2c-outfile filter

--- out2c.sh
+++ out2c.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-. `dirname $0`/config.pipe
+. $(dirname $0)/config.pipe
 ###
+
+if [[ -z "$1" || -z "$2" || -n "$3" ]]
+then
+    echo Usage:$(basename $0) DIR_WITH_FILES FILE.clf
+    exit 1
+fi
+
+OUT=$1
+CLF=$2
+SCALE=${SVMIN}/xy2-bte-dom-txtf.allvecs.srange
+MODEL=${MODELS}/best
 
 function out2train
 {
     echo -n "${1// /-}: "
     count=0
-    for g in $OUT/*.gold
+    for i in $(cut -d ' ' -f 1 ${CLF})
     do
         let count++
-        i=$(basename $g .gold)
 
-        if [[ ! -f  ${SVMIN}2c/$i.${1// /-}.vecs ]]
+        if [[ ! -f  $OUT/$i.${1// /-}.vecs ]]
         then
-        
-            eval paste -d \' \' $(for j in $1; do echo '<(cat '"$OUT/$i.$j"') '; done;) | ./vectorize2c.py > ${SVMIN}2c/$i.${1// /-}.vecs
+            eval paste -d \' \' $(for j in $1; do echo '<(cat '"$OUT/$i.$j"') '; done;) | ${BASE}/vectorize2c.py > ${OUT}/$i.${1// /-}.vecs
+            svm-scale -r $SCALE ${OUT}/$i.${1// /-}.vecs > ${OUT}/$i.${1// /-}.scaled
+        else
+            echo -n "."
+        fi
+
+        if [[ -f ${OUT}/$i.${1// /-}.scaled && ! -f $i.pred ]]
+        then
+            svm-predict ${OUT}/$i.${1// /-}.scaled $MODEL $OUT/$i.pred > /dev/null 2>&1
         else
             echo -n "."
         fi
@@ -23,5 +40,4 @@ function out2train
     echo "${count}"
 }
 
-out2train "viz dom"
-out2train "viz dom txtfeat"
+out2train "xy2 bte dom txtf"
